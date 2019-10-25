596a8308dae80ce2f6184531f8b6ddad8ef6930e
Jimmy Zhang <jimmy.zhang@sisense.com>
2019-10-25 15:35:13 -0700

    19/20 first commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 $( document ).ready(function() {
-	var url = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2018-19&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=&callback=?"
-	var rodger_players = ["Trae Young", "Collin Sexton", "Marvin Bagley III", "Wendell Carter Jr.", "Shai Gilgeous-Alexander"]
-	var jimmy_players = ["Luka Doncic", "Deandre Ayton", "Kevin Knox", "Jaren Jackson Jr.", "Miles Bridges"]
+	var url = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2019-20&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight="
+	var rodger_players = ["RJ Barrett", "Zion Williamson", "Rui Hachimura", "De'Andre Hunter", "Tyler Herro"]
+	var jimmy_players = ["Ja Morant", "Darius Garland", "Coby White", "Brandon Clarke", "Jarrett Culver"]
 
 	$.getJSON(url,  // url
 	    function (data) {  // success callback
